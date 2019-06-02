f6ff5e9c9c077f46fac92a26fe756ad2e725cb41
andrzej-tests <andrzej.boguslaw.wisniewski@gmail.com>
2019-06-03 00:59:04 +0200

    add role for k8s to pull from gcr

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
     "roles/logging.logWriter",
     "roles/logging.viewer",
     "roles/monitoring.metricWriter",
+    "roles/storage.objectViewer",
   ]
 
   name = "${var.id == "" ? var.environment : format("%s-%s", var.environment, var.id)}"
